e20731612bada46c2e9c85ee1cf319cf1945ff34
Danny12123 <danielacquah579@gmail.com>
2025-08-18 16:19:01 +0000

    Add new dependencies and enhance contact form functionality with toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,9 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import "aos/dist/aos.css";
+import { Toaster } from "sonner"
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+
 
 const geistMono = Montserrat({
   weight: ["300", "400", "700", "900"],
@@ -19,6 +17,46 @@ const geistMono = Montserrat({
 export const metadata: Metadata = {
   title: "Inkhaus Gh",
   description: "We are more than just a printing company, we are creators!Born in Ghana and driven by expression, Ink Haus GH exists to help businesses, events, and individuals stand out.",
+  keywords:
+    "printing, custom printing, Ghana printing, Inkhaus Gh, creative printing, business printing, event printing, personalized printing, high-quality prints, design and print, professional printing services",
+  authors: [{ name: "Inkhaus Gh" }],
+  creator: "Daniel Acquah",
+  publisher: "Inkhaus Gh",
+  formatDetection: {
+    email: false,
+    telephone: false
+  },
+  metadataBase: new URL("https://www.linkhusgh.com"),
+  alternates: {
+    canonical: "/"
+  },
+  openGraph: {
+    title: "Inkhaus Gh",
+    description:
+      "We are more than just a printing company, we are creators!Born in Ghana and driven by expression, Ink Haus GH exists to help businesses, events, and individuals stand out.",
+    url: "https://www.linkhusgh.com",
+    siteName: "Inkhaus Gh",
+    images: [
+      {
+        url: "/Image/logo.svg",
+        width: 1200,
+        height: 630,
+        alt: "Inkhaus Gh"
+      }
+    ],
+    locale: "en_US",
+    type: "website"
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-image-preview": "large",
+      "max-snippet": -1
+    }
+  }
 };
 
 export default function RootLayout({
@@ -32,6 +70,7 @@ export default function RootLayout({
         className={` ${geistMono.variable} antialiased scroll-smooth`}
       >
         {children}
+         <Toaster richColors />
       </body>
     </html>
   );
